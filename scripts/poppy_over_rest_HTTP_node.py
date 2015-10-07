30ee7ef20f795e07e3e36e0e9045b603c3d6a800
Manon Cortial <mc@generationrobots.com>
2015-10-07 16:22:26 +0200

    rest-HTTP node updated for JointState

--- scripts/poppy_over_rest_HTTP_node.py
+++ scripts/poppy_over_rest_HTTP_node.py
@@ -1,88 +1,142 @@
 #!/usr/bin/env python
 
 
+#~ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#~ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#~ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#~ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#~ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+#~ OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+#~ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 #~ https://github.com/pierre-rouanet/pypot/blob/master/REST-APIs.md
 
 import rospy
-from std_msgs.msg import String, Float32MultiArray, MultiArrayDimension
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 
 import urllib2, json
 
 #~ registers = ["registers", "goal_speed", "compliant", "safe_compliant", "angle_limit", "present_load", "id", "present_temperature", "moving_speed", "torque_limit", "goal_position", "upper_limit", "lower_limit", "name", "present_speed", "present_voltage", "present_position", "model", "compliance_slope", "compliance_margin"]
-read_registers = ["goal_speed", "compliant", "present_temperature",  "goal_position",   "present_speed",  "present_position", "id"]
-write_registers = ["goal_speed", "compliant", "goal_position"]
+#~ read_registers = ["goal_speed", "compliant", "present_temperature",  "goal_position",   "present_speed",  "present_position", "id"]
+#~ write_registers = ["goal_speed", "compliant", "goal_position"]
 
+global robot_address
 robot_address = 'http://poppy.local:8080'
 
-def setRegister(data, register):
+def getRegister(motor, register):
+    pos = urllib2.urlopen(robot_address + "/motor/"+motor+"/register/"+register).read()
+    pos = json.loads(pos)[register]
+    return pos
     
-    #~ r = data._connection_header['topic']
-    #~ r= r.replace("/poppy/register/", "")
-    #~ r = r.replace("/write", "")
-    #~ print "register ",r
-    
-    register = register[0]
-    
-    for i in range(0, len(data.layout.dim)):
-        m = data.layout.dim[i].label
-        print "motor ",m
-        v = data.data[i]
-        if register == "compliant":
-            v = v!= 0.
-        print "value ",v
+def setRegister(motor, register, data):
+    values = json.dumps(data)
+    url = robot_address +'/motor/'+motor+'/register/'+register+'/value.json'
+    req = urllib2.Request(url, values)
+    req.add_header("Content-Type",'application/json')
+    response = urllib2.urlopen(req)
+
+
+
+
+def JointStateWrite(data):
+    print data
+    for i in range(len(data.name)):
+
+        if len(data.effort) > 0:
+            setRegister(data.name[i], "torque_limit" , data.effort[i])
+            if  data.effort[i] > 0.:
+                setRegister(data.name[i], "compliant" , False)
+            else:
+                setRegister(data.name[i], "compliant" , False)
+        if len(data.position) > 0:
+            setRegister(data.name[i], "goal_position" , data.position[i])
+        if len(data.velocity) > 0:
+            setRegister(data.name[i], "goal_speed" , data.velocity[i])
+
         
-        url = robot_address +'/motor/'+m+'/register/'+register+'/value.json'
-
-        values = json.dumps(v)
-
-        req = urllib2.Request(url, values)
-        req.add_header("Content-Type",'application/json')
-        response = urllib2.urlopen(req)
-
         
-#~ def usePrimitive(data, args):
+def usePrimitive(data, args):
 
-    #~ p = getattr(poppy, args[0])
-    #~ if args[1] == "start":
-        #~ p.start()
-    #~ else:
-        #~ p.stop()
+    if args[1] == "start":
+        url = robot_address +'/primitive/'+args[0]+'/start.json'
+    else:
+        url = robot_address +'/primitive/'+args[0]+'/stop.json'
+
+    pos = urllib2.urlopen(url).read()
+    
+        
 
 def poppy_node_rest():
     rospy.init_node('poppy_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     
+    ip = rospy.get_param(rospy.get_name()+'/ip','poppy.local')
+    port = rospy.get_param(rospy.get_name()+'/port','8888')
+    
+    global robot_address
+    robot_address = 'http://'+ip+':'+str(port)
+    print "connecting to "+robot_address
+    
     motorList = urllib2.urlopen(robot_address +"/motor/list.json").read()
     motorList = json.loads(motorList)["motors"]
     
-    #~ pubs = {}
-    #~ for m in motorList:
-        #~ pubs[m] = rospy.Publisher('poppy/motor/'+m+'/read', Float32MultiArray, queue_size=10)
- 
-    pubs = {}
-    for r in read_registers:
-        pubs[r] = rospy.Publisher('poppy/register/'+r+'/read', Float32MultiArray, queue_size=10)
+    #set motors info as params
+    for m in motorList:
 
+        rospy.set_param(rospy.get_name()+'/motor/'+m+'/id', getRegister(m, "id"))
+        rospy.set_param(rospy.get_name()+'/motor/'+m+'/model', getRegister(m, "model"))
+        #~ rospy.set_param(rospy.get_name()+'/motor/'+m.name+'/direct', m.direct)
+        #~ rospy.set_param(rospy.get_name()+'/motor/'+m.name+'/offset', m.offset)
+        rospy.set_param(rospy.get_name()+'/motor/'+m+'/upper_limit', getRegister(m, "upper_limit"))
+        rospy.set_param(rospy.get_name()+'/motor/'+m+'/lower_limit', getRegister(m, "lower_limit"))
         
-    for r in write_registers:
-        rospy.Subscriber('poppy/register/'+r+'/write', Float32MultiArray, setRegister, callback_args=[r])
+    #create publishers for motors present_position, present_speed, present_load and goal_position and goal_speed and max_torque
+    pubs = {}
+
+    pubs[rospy.get_name()+'/motors/read_present'] = rospy.Publisher(rospy.get_name()+'/motors/read_present', JointState, queue_size=10)
+    pubs[rospy.get_name()+'/motors/read_goal'] = rospy.Publisher(rospy.get_name()+'/motors/read_goal', JointState, queue_size=10)
+
+     #subscribe to topic to change the goal_position, goal_speed and compliance
+    rospy.Subscriber(rospy.get_name()+'/motors/write', JointState, JointStateWrite)   
+    
+    primitivesList = urllib2.urlopen(robot_address +"/primitive/list.json").read()
+    primitivesList = json.loads(primitivesList)["primitives"]
+    
+    for p in primitivesList:
+        rospy.Subscriber(rospy.get_name()+'/primitive/'+p+'/start', String, usePrimitive, callback_args=[p, "start"])   
+        rospy.Subscriber(rospy.get_name()+'/primitive/'+p+'/stop', String, usePrimitive, callback_args=[p, "stop"])  
     
     while not rospy.is_shutdown():
-        for r in read_registers:
+            
+        msg = JointState()
+        msg.name = []
+        msg.position = []
+        msg.velocity = []
+        msg.effort = []
+
+        for m in motorList:           
+            msg.name.append(m)
+            msg.position.append(getRegister(m, "present_position"))
+            msg.velocity.append(getRegister(m, "present_speed"))
+            msg.effort.append(getRegister(m, "present_load"))
+
+        pubs[rospy.get_name()+'/motors/read_present'].publish(msg)
         
-            msg = Float32MultiArray()
-            msg.data = []
-            
-            for m in motorList:
-                pos = urllib2.urlopen(robot_address + "/motor/"+m+"/register/"+r).read()
-                pos = json.loads(pos)[r]
-                msg.data.append(pos)
-                dimension = MultiArrayDimension()
-                dimension.label=m
-                msg.layout.dim.append(dimension)
-            
-            
-            pubs[r].publish(msg)
+        
+        msg = JointState()
+        msg.name = []
+        msg.position = []
+        msg.velocity = []
+        msg.effort = []
+
+        for m in motorList:           
+            msg.name.append(m)
+            msg.position.append(getRegister(m, "goal_position"))
+            msg.velocity.append(getRegister(m, "goal_speed"))
+            msg.effort.append(getRegister(m, "torque_limit"))
+
+        pubs[rospy.get_name()+'/motors/read_goal'].publish(msg)
 
         rate.sleep()
 
